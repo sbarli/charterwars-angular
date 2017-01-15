a031da9e2c9fcd54f58a495d4e0f650f7d1956b3
samantha <samanthasalley@gmail.com>
2017-01-15 09:15:14 +0000

    Add form to debate html; add form post function to debate controller; Remove placeholder question css

--- public/angular-app/components/debate/debate-controller.js
+++ public/angular-app/components/debate/debate-controller.js
@@ -1,6 +1,6 @@
 angular.module('charterwars').controller('DebateController', DebateController);
 
-function DebateController($uibModal, $location, anchorSmoothScroll){
+function DebateController($http, $uibModal, $location, anchorSmoothScroll){
     var vm = this;
     
     vm.openModal = function(){
@@ -22,8 +22,60 @@ function DebateController($uibModal, $location, anchorSmoothScroll){
     
     // vm.openModal();
     
-    vm.goToVids = function (eID){
-      $location.hash(eID);
-      anchorSmoothScroll.scrollTo(eID);
+    vm.goToVids = function (index){
+        var next_idx;
+        if(index === 'first'){
+            next_idx = 0;
+        }else{
+            next_idx = index + 1;
+        }
+        var eID = 'debate-vid-' + next_idx;
+        $location.hash(eID);
+        anchorSmoothScroll.scrollTo(eID);
     };
+    
+    vm.post = function(section){
+        var post = {
+            name: vm.name,
+            answer: section.answer,
+            question: section.id
+        };
+        
+        console.log('post', post);
+        
+        if(!section.answer){
+            section.error = 'Please add an answer!';
+        }else{
+            $http.post('/api/posts', post).then(function(result){
+                console.log(result);
+                section.success = 'Successful post!';
+                section.error = null;
+            }).catch(function(err){
+                console.log(err);
+                section.error = 'There was an error. Please try again.';
+            });
+        }
+    };
+    
+    vm.sections = [
+        {
+            "title": "Choice",
+            "question": "Are charter schools really a choice?",
+            "url": "https://player.vimeo.com/video/182436417",
+            "id": 1
+        },  
+        {
+            "title": "Competition",
+            "question": "Is competition important in education?",
+            "url": "https://player.vimeo.com/video/182436415",
+            "id": 2
+        },  
+        {
+            "title": "Innovation",
+            "question": "Is innovation important in education?",
+            "url": "https://player.vimeo.com/video/182436415",
+            "id": 3
+        },    
+    ];
+    
 }

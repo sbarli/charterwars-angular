f36d29a8cb56a69cefc394d86aea2631e77f7c7c
samantha <samanthasalley@gmail.com>
2017-02-20 01:09:25 +0000

    Update city page layout and ctrl to mirror debate page

--- public/angular-app/components/city/city-controller.js
+++ public/angular-app/components/city/city-controller.js
@@ -1,12 +1,60 @@
 angular.module('charterwars').controller('CityController', CityController);
 
-function CityController($location, anchorSmoothScroll, $scope){
+function CityController($location, anchorSmoothScroll, pageDataFactory, $filter, $route, $scope, pageSectionColorService){
     $scope.showNavigation(true);
     
     var vm = this;
     
-    vm.goToVids = function (eID){
-      $location.hash(eID);
-      anchorSmoothScroll.scrollTo(eID);
+    vm.pageDwnArrow = 'angular-app/assets/content-nav/city-down-outline.png';
+    vm.pageUpArrow = 'angular-app/assets/content-nav/city-up-outline.png';
+    
+    vm.colors = pageSectionColorService.getIndexColors();
+    
+    pageDataFactory.pageList().then(function(response){
+        var pages = response.data;
+        vm.cityPage = ($filter('filter')(pages, {"name":"The City"}))[0];
+        angular.forEach(vm.cityPage.sections, function(section, i){
+            section.background = vm.colors[i];
+        });
+    });
+    
+    vm.goToVids = function (index){
+        var next_idx;
+        if(index === 'first'){
+            next_idx = 0;
+        }else{
+            next_idx = index + 1;
+        }
+        var eID = 'city-vid-' + next_idx;
+        anchorSmoothScroll.scrollTo(eID);
     };
+    
+    vm.backToTop = function (eID){
+        anchorSmoothScroll.scrollTo(eID);
+    };
+    
+    vm.post = function(section){
+        var pageId = vm.cityPage._id;
+        var sectionId = section._id;
+        var responseData = {
+            name: vm.name,
+            answer: section.answer
+        };
+        
+        console.log('responseData', responseData);
+        
+        if(!section.answer){
+            section.error = 'Please add an answer!';
+        }else{
+            pageDataFactory.postResponse(pageId, sectionId, responseData).then(function(response){
+                console.log(response.status);
+                if(response.status === 201){
+                    section.responded = true;
+                }
+            }).catch(function(err){
+                console.log(err);
+            });
+        }
+    };
+    
 }
